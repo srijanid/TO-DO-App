4bca9a05f6cd9645dda7b969740e623381b50925
srijanidas06 <srijanidas06@gmail.com>
2024-08-09 13:04:56 +0000

    first commit

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'my_button.dart';
+
+// ignore: must_be_immutable
+class DialogBox extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: Container(
+        padding: const EdgeInsets.only(left: 5.0, right: 5.0,top: 10.0),
+        height: 150,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // get user input
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new task",
+              ),
+            ),
+
+            // buttons -> save + cancel
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // save button
+                MyButton(text: "Save", onPressed: onSave),
+
+                const SizedBox(width: 8),
+
+                // cancel button
+                MyButton(text: "Cancel", onPressed: onCancel),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
